bc22edfed3ed1e3ad1edb2bdfe8c0d1e78f67cec
Joel Hench <joelhench@Olivias-MacBook-Pro-3.local>
2022-05-05 10:27:38 -0700

    carouselbeta

--- client/src/components/Drone.jsx
+++ client/src/components/Drone.jsx
@@ -85,6 +85,34 @@ class Drone extends React.Component {
           </div>
           )}
         )}
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="/DronePhoto/DJI_0273.JPG" alt="First slide"/>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/DronePhoto/DJI_0110.JPG" alt="Second slide"/>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/DronePhoto/DJI_0378.JPG" alt="Third slide"/>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
         </div>
         </div>
     )

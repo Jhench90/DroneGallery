8ee71396c2bab780ad59db00ad75846e7da019f5
Joel Hench <joelhench@Olivias-MacBook-Pro-3.local>
2022-06-02 15:54:26 -0700

    public ips updated

--- client/src/components/Blogs.jsx
+++ client/src/components/Blogs.jsx
@@ -42,23 +42,23 @@ class Blogs extends React.Component {
             })}
           </div>
         </div>
-        <div style={{ width: '60%', float: 'left', backgroundColor: 'white'}}>
+        <div style={{ width: '60%', float: 'left', backgroundColor: 'white' }}>
           <div style={{ height: '400px', overflow: 'hidden' }}>
             <img
-            // style={{ marginTop: '-150px' }}
-              src={this.state.post.banner} style={{width: '100%'}} />
-            <h1 style={{ position: 'absolute', top: '25%', left: '34%', size: '20p', width: '40%', color: 'white', textAlign: 'center'}}>How to deploy your personal website on your old laptop</h1>
+              // style={{ marginTop: '-150px' }}
+              src={this.state.post.banner} style={{ width: '100%' }} />
+            <h1 style={{ position: 'absolute', top: '25%', left: '34%', size: '20p', width: '40%', color: 'white', textAlign: 'center' }}>How to deploy your personal website on your old laptop</h1>
           </div>
           <div>
-            <div style={{textAlign: 'center'}}>
+            <div style={{ textAlign: 'center' }}>
               Author: Jay
             </div>
-            <div style={{textAlign: 'center'}}>
+            <div style={{ textAlign: 'center' }}>
               Date: 5/18/2022
             </div>
             <div>
               <p>
-                Deploying a server on your old laptop offers advantages and disadvantages to deploying over a standard cloud based service like AWS or Azure. One advantage is you will not be charged for memory usage or computing time. A disadvantage is your download and upload speeds can be limited by the plan you are signed up for with your ISP. The laptop would be running at all times and the electricity bill can increase. In either case, if you wish to register a domain name, you would still need to pay a yearly fee and end up paying some money in the end.
+                Deploying a server on your old laptop offers advantages and disadvantages to deploying over a standard cloud based service like AWS or Azure. One advantage is you will not be charged for memory usage or computing time. A disadvantage is your download and upload speeds can be limited by the plan you are signed up for with your ISP. The laptop would be running at all times and the electricity bill can increase. Additionally, configuring a home server requires some knowledge of DNS, IPs, router settings, and port-forwarding. In either case, if you wish to register a domain name, you would still need to pay a yearly fee and end up paying some money in the end.
               </p>
               <p>
                 The following steps were followed for a PC based deployment.
@@ -80,24 +80,34 @@ class Blogs extends React.Component {
                   5. Create directories and clone repository.
                 </li>
                 <li>
-                  6. Run installation commands are necessary for your server. ( npm install, npm run react-dev, npm run start to start server listening ).
+                  6. Run installation commands as necessary for your server. ( npm install, npm run react-dev, npm run start to start server listening ).
                 </li>
                 <li>
-                  7. Find your personal computer IP address through checking Wifi connection settings. https://support.microsoft.com/en-us/windows/find-your-ip-address-in-windows-f21a9bbc-c582-55cd-35e0-73431160a1b9
+                  7. The traffic routing design is as follows:
+                Domain name --> (port-forwarded) --> Router public IPv4 --> (port-forwarded) --> Server computer private IPv4.
                 </li>
                 <li>
-                  8. Edit inbound rules in Firewall to allow access through the port your server listens at ( 3000 in most cases ).
+                   8. To determine your server computers IP, open up cmd in start menu, and enter the command >insert, the >insert is your computer server IP address. Alternatively, find the IP through checking Wifi connection settings. https://support.microsoft.com/en-us/windows/find-your-ip-address-in-windows-f21a9bbc-c582-55cd-35e0-73431160a1b9
                 </li>
                 <li>
-                  9. The server is immediately reachable at your PCs IP address and port. ( http://192.168.0.24:3000/ )
+                  9. Edit inbound rules in Firewall to allow access through the port your server listens at ( 3000 in most cases ).
                 </li>
                 <li>
-                  10. Finally, set up port forwarding on Godaddy using your personal computer IP address and port, so traffic arriving at your domain name is forwarded with masking to your IP address port. ( jayandthesky.me → http://192.168.0.24:3000/ ). Port forwarding can take 1 hour up to 48 hours to take effect.
+                  10. The server is immediately reachable on your WiFi at your IP address + port. ( http://192.168.0.24:3000/ )
+                </li>
+                <li>
+                  11. To open up a port on your router, log in to your routers GUI. How to access your router GUI is normally printed somewhere on the router. Otherwise, visiting 192.168.0.0 or 192.168.0.1 may allow you to access the log in. >insert. Create a new Port-forward to recieve connections on port 3000 and forward traffic to your server computer IP + port.
+                </li>
+                <li>
+                  12. Finally, set up port forwarding with masking on Godaddy in your DNS settings to send traffic to your router IP + port. Visit https://www.whatismyip.com/ while connected to your Wifi to determine your routers public IP address. ( jayandthesky.me → http://67.161.47.190:3000/ ). Godaddy Port forwarding can take 1 hour up to 48 hours to take effect. Your router Port Forward should take effect immediately.
                 </li>
               </ul>
               <p>
                 Thats it! Your website is now up and running!
               </p>
+              <p>Additional Notes:</p>
+              <p>Based on this design, your website can be accesssed several ways. While on the WiFi network, you may connect to your website through the server computer IP, router IP, or domain name. While off the WiFi network, you may connect to your website through the router IP or domain name. </p>
+              <p>Your server computer and router have public and private IP addresses which should not be mixed up. The public address is reachable by users not connected to your local network. The private IP address is created for each device by your router in numerical order, with the router commonly starting at 192.168.0.0 or 192.168.0.1, and following devices in order (192.168.0.2, 192.168.0.3, 192.168.0.4, etc). All your devices, laptops, phones, Google Home, are all assigned a private IP address. A user not connected to your WiFi/local network is unable to reach your server computer via the private/local IP address.</p>
               <div>
                 Tags: Deployment, Windows, IP, DNS
               </div>
--- client/src/components/MusicDigs.jsx
+++ client/src/components/MusicDigs.jsx
@@ -8,10 +8,9 @@ class MusicDigs extends React.Component {
   render () {
     return (
       <div style={{zIndex: '-1', position: 'absolute', left:'35%', width: '50%'}}>
-        <div>Curtis Harding ft. Jazmine Sullivan - Our Love </div>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/2Yr3sKPi8mM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <div>Nina Simone - Sinnerman (Sofi Tukker Remix)</div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/mmhoLkgGWhw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/mmhoLkgGWhw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/SDNA934EEVk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <div style={{width: '400px', textAlign: 'center'}}>© 2022 Jay.andthesky. Made possible with React.js, Bootstrap, ArangoDB, and DJI.</div>
       </div>
     )

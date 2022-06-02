3b63d347cebb4e22a71fe12db1c255a782ad8b59
Joel Hench <joelhench@Olivias-MacBook-Pro-3.local>
2022-06-01 19:12:45 -0700

    markdown language

--- client/src/components/Blogs.jsx
+++ client/src/components/Blogs.jsx
@@ -4,7 +4,7 @@ class Blogs extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: ['Website Deployment - 5/18/22', 'test2', 'test3'],
+      posts: ['Website Deployment - 5/18/22'],
       post: {
         banner: '/DronePhoto/DJI_0120.JPG', shorttitle: 'website deployment', title: 'How to deploy your personal website on your old laptop', author: 'Jay', date: '5/18/2022', content: `Deploying a server on your old laptop offers advantages and disadvantages to deploying over a standard cloud based service like AWS or Azure. One advantage is you will not be charged for memory usage or computing time. A disadvantage is your download and upload speeds can be limited by the plan you are signed up for with your ISP. The laptop would be running at all times and the electricity bill can increase. In either case, if you wish to register a domain name, you would still need to pay a yearly fee and end up paying some money in the end.
         The following steps were followed for a PC based deployment.
@@ -19,7 +19,8 @@ class Blogs extends React.Component {
           9. The server is immediately reachable at your PCs IP address and port. ( http://192.168.0.24:3000/ )
         10. Finally, set up port forwarding on Godaddy using your personal computer IP address and port, so traffic arriving at your domain name is forwarded with masking to your IP address port. ( jayandthesky.me → http://192.168.0.24:3000/ ). Port forwarding can take 1 hour up to 48 hours to take effect.
 
-        Thats it! Your website is now up and running!      ` }
+        Thats it! Your website is now up and running!      `, tags: ['Deployment', 'Windows', 'IP', 'DNS']
+      }
     }
   };
   render() {
@@ -30,6 +31,7 @@ class Blogs extends React.Component {
         <div style={{ width: '20%', float: 'left', marginLeft: '5%', textDecoration: 'underline' }}>
           <div id="postlinks" style={{ float: 'left', width: '12%' }}>
             <div className="triplinks" style={{ textDecoration: 'underline' }} >Posts</div>
+            {/* <button style={{ whiteSpace: 'nowrap' }}>Add New</button> */}
 
             {this.state.posts.map((post) => {
               return (
@@ -43,16 +45,79 @@ class Blogs extends React.Component {
         <div style={{ width: '60%', float: 'left' }}>
           <div style={{ overflow: 'hidden' }}>
             <img style={{ marginTop: '-150px' }} width='1000px' src={this.state.post.banner} />
-            {this.state.post.banner}
+            <h1 style={{ position: 'absolute', top: '25%', left: '34%', size: '20p', width: '50%', color: 'white'}}>How to deploy your personal website on your old laptop</h1>
           </div>
           <div>
-            Title: {this.state.post.title}
+            <div style={{textAlign: 'center'}}>
+              Author: Jay
+            </div>
+            <div style={{textAlign: 'center'}}>
+              Date: 5/18/2022
+            </div>
+            <div>
+              <p>
+                Deploying a server on your old laptop offers advantages and disadvantages to deploying over a standard cloud based service like AWS or Azure. One advantage is you will not be charged for memory usage or computing time. A disadvantage is your download and upload speeds can be limited by the plan you are signed up for with your ISP. The laptop would be running at all times and the electricity bill can increase. In either case, if you wish to register a domain name, you would still need to pay a yearly fee and end up paying some money in the end.
+              </p>
+              <p>
+                The following steps were followed for a PC based deployment.
+              </p>
+              <ul>
+                <li>
+                  1. Purchase domain name through Godaddy.
+                </li>
+                <li>
+                  2. Use Node.js installer to install Node.js and NPM. Choose carefully during installation to ensure NPM is added. https://nodejs.org/en/download/
+                </li>
+                <li>
+                  3. Install NVM for Windows. https://github.com/coreybutler/nvm-windows/releases
+                </li>
+                <li>
+                  4. Install Git for Windows. https://git-scm.com/download/win
+                </li>
+                <li>
+                  5. Create directories and clone repository.
+                </li>
+                <li>
+                  6. Run installation commands are necessary for your server. ( npm install, npm run react-dev, npm run start to start server listening ).
+                </li>
+                <li>
+                  7. Find your personal computer IP address through checking Wifi connection settings. https://support.microsoft.com/en-us/windows/find-your-ip-address-in-windows-f21a9bbc-c582-55cd-35e0-73431160a1b9
+                </li>
+                <li>
+                  8. Edit inbound rules in Firewall to allow access through the port your server listens at ( 3000 in most cases ).
+                </li>
+                <li>
+                  9. The server is immediately reachable at your PCs IP address and port. ( http://192.168.0.24:3000/ )
+                </li>
+                <li>
+                  10. Finally, set up port forwarding on Godaddy using your personal computer IP address and port, so traffic arriving at your domain name is forwarded with masking to your IP address port. ( jayandthesky.me → http://192.168.0.24:3000/ ). Port forwarding can take 1 hour up to 48 hours to take effect.
+                </li>
+              </ul>
+              <p>
+                Thats it! Your website is now up and running!
+              </p>
+              <div>
+                Tags: Deployment, Windows, IP, DNS
+              </div>
+            </div>
+          </div>
+
+          {/* <div>
+            {this.state.post.title}
             <br></br>
             Author: {this.state.post.author} - {this.state.post.date} </div>
-          <br></br>
-          {splitted.map(function (split) {
-            return (<p>{split}</p>)
-          })}
+          <br></br> */}
+          {/* {splitted.map(function (split) {
+            return (
+              <div>
+                <p>{split}</p>
+                <ul>
+                  <li>{split}
+                  </li>
+                </ul>
+              </div>)
+          })} */}
+
           <br></br>
         </div>
       </div>
